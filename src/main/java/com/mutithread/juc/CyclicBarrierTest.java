1dca466303e2daf99db7468d986bc398c048ef62
李浩 <lihao@jsb5.com>
2021-02-26 16:51:49 +0800

    location:

--- src/main/java/com/mutithread/juc/CyclicBarrierTest.java
+++ src/main/java/com/mutithread/juc/CyclicBarrierTest.java
@@ -1,12 +1,15 @@
 package com.mutithread.juc;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Date;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
+@Slf4j
 public class CyclicBarrierTest {
     public static void main(String[] args) {
-        CyclicBarrier barrier = new CyclicBarrier(2);  // 人满发车
+        CyclicBarrier barrier = new CyclicBarrier(3);  // 人满发车
 
         new Thread(() -> {
             System.out.println("t1开始... " + new Date());
@@ -20,17 +23,24 @@ public class CyclicBarrierTest {
 
         new Thread(() -> {
             System.out.println("t2开始... " + new Date());
+
             try {
                 Thread.sleep(2000L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            try {
                 barrier.await();
             } catch (InterruptedException | BrokenBarrierException e) {
                 e.printStackTrace();
             }
             System.out.println("t2向下继续... " + new Date());
         }, "t2").start();
+
+        new Thread(() -> {
+            System.out.println("t3开始... " + new Date());
+            try {
+                barrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+            System.out.println("t3向下继续... " + new Date());
+        }, "t3").start();
     }
 }
